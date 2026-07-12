87309d5b2e950d8b216bba68ecf02b325ee485d5
kelly26ici <rexk638@gmail.com>
2026-07-12 10:09:41 +0300

    Fix FastAPI app initialization

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
+from fastapi import FastAPI
+
 from src.routes.webhook import router as webhook_router
+
+app = FastAPI(
+    title="Samantha API",
+    version="1.0.0",
+)
+
 app.include_router(webhook_router)
